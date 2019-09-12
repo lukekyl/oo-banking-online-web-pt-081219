b5a35ad7687b1d79bc28aa25f7b613c3a502cc57
Kyle Luke <th.oldschool@gmail.com>
2019-09-12 02:30:29 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
   attr_accessor :status
-  attr_reader :name, :balance
+  attr_reader :name
   
   def initialize(name)
     @name = name
@@ -10,5 +10,9 @@ class BankAccount
   
   def deposit(ammount)
     @balance += ammount
+  end
+  
+  def display_balance
+    @balance
   end
 end
